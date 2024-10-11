9bc9c00d35e3f1f7a6371193e3ca9a0e8ea27785
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-11 10:57:12 +0430

    scrollable welcome screen

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,33 +1,46 @@
 import react from "react";
 
-const { Text, View } = require("react-native");
+const { Text, View, ScrollView } = require("react-native");
 
 export default function WelcomeScreen() {
   return (
     <View style={{ flex: 1 }}>
-      <Text
-        style={{
-          padding: 40,
-          fontSize: 30,
-          color: "#EDEFEE",
-          textAlign: "center",
-        }}
-      >
-        Welcome to Little Lemon
-      </Text>
-      <Text
-        style={{
-          fontSize: 24,
-          color: "white",
-          padding: 20,
-          marginVertical: 8,
-          textAlign: "center",
-        }}
-      >
-        Little Lemon is a charming neibhorhood bistro that serves simple food
-        and classic food in a lively but casual environment. we would love to
-        hear more about your experience with us!
-      </Text>
+      <ScrollView indicatorStyle={"white"}>
+        <Text
+          style={{
+            padding: 40,
+            fontSize: 30,
+            color: "#EDEFEE",
+            textAlign: "center",
+          }}
+        >
+          Welcome to Little Lemon
+        </Text>
+        <Text
+          style={{
+            fontSize: 24,
+            color: "white",
+            padding: 20,
+            marginVertical: 8,
+            textAlign: "center",
+          }}
+        >
+          Little Lemon is a charming neibhorhood bistro that serves simple food
+          and classic food in a lively but casual environment. we would love to
+          hear more about your experience with us! Little Lemon is a charming
+          neibhorhood bistro that serves simple food and classic food in a
+          lively but casual environment. we would love to hear more about your
+          experience with us! Little Lemon is a charming neibhorhood bistro that
+          serves simple food and classic food in a lively but casual
+          environment. we would love to hear more about your experience with us!
+          Little Lemon is a charming neibhorhood bistro that serves simple food
+          and classic food in a lively but casual environment. we would love to
+          hear more about your experience with us! Little Lemon is a charming
+          neibhorhood bistro that serves simple food and classic food in a
+          lively but casual environment. we would love to hear more about your
+          experience with us!
+        </Text>
+      </ScrollView>
     </View>
   );
 }
