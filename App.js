49e87a0b180e6ba2f8ba35ec88dbf23017cd1a8a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-19 17:15:49 +0430

    Handling Text Input

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
-const YourApp = () => {
+const PizzaTranslator = () => {
+  const [text, setText] = useState("");
   return (
     <View
       style={{
-        flex: 1,
+        padding: 10,
         justifyContent: "center",
         alignItems: "center",
+        flex: 1,
       }}
     >
-      <Text>Try editing me! 🎉</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Type here to translate!"
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{ padding: 10, fontSize: 42 }}>
+        {text
+          .split(" ")
+          .map((word) => word && "🍕")
+          .join(" ")}
+      </Text>
     </View>
   );
 };
 
-export default YourApp;
+export default PizzaTranslator;
