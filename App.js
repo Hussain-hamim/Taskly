f7f73f5d8c5c3fc6dec11759c049b0f961015246
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-19 22:31:23 +0430

    platform specific code

--- App.js
+++ App.js
@@ -1,55 +1,19 @@
-import React from "react";
-import { SectionList, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22,
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: "bold",
-    backgroundColor: "rgba(247,247,247,1.0)",
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+    ...Platform.select({
+      ios: {
+        backgroundColor: "red",
+      },
+      android: {
+        backgroundColor: "green",
+      },
+      default: {
+        // other platforms, web for example
+        backgroundColor: "blue",
+      },
+    }),
   },
 });
-
-const SectionListBasics = () => {
-  return (
-    <View style={styles.container}>
-      <SectionList
-        sections={[
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-          { title: "A", data: ["a", "b", "c"] },
-        ]}
-        renderItem={({ item }) => <Text style={styles.item}> {item}</Text>}
-        renderSectionHeader={({ section }) => (
-          <Text style={styles.sectionHeader}>{section.title}</Text>
-        )}
-        keyExtractor={(item) => item}
-      />
-    </View>
-  );
-};
-
-export default SectionListBasics;
