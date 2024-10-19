086d78b12fa25e43e6c54b59e2f6f00a8c9a4a95
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-19 18:00:23 +0430

    On iOS a ScrollView with a single item can be used to allow the user to zoom content

--- App.js
+++ App.js
@@ -9,36 +9,6 @@ const logo = {
 
 const App = () => (
   <ScrollView pagingEnabled={true}>
-    <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{ fontSize: 96 }}>If you like</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{ fontSize: 96 }}>Scrolling down</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{ fontSize: 96 }}>What's the best</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{ fontSize: 96 }}>Framework around?</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
     <Text style={{ fontSize: 80 }}>React Native</Text>
   </ScrollView>
 );
