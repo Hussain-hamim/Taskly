fed068fb769d0c5c2088a1e8d1b4f5f308e2850a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-22 22:51:26 +0430

    debugging in vs code

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Alert, Button, Text, View } from "react-native";
 
 const YourApp = () => {
+  let x = 1;
+
+  console.log("hello world");
+
   return (
     <View
       style={{
