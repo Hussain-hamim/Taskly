9e54673c05c6c0545321643ef4e55029a3e56d3d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-24 13:21:02 +0430

    Absolute & Relative Layout

--- App.js
+++ App.js
@@ -1,64 +1,73 @@
 import React, { useState } from "react";
-import {
-  View,
-  SafeAreaView,
-  TouchableOpacity,
-  Text,
-  StyleSheet,
-} from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
-const WidthHeightBasics = () => {
-  const [widthType, setWidthType] = useState("auto");
-  const [heightType, setHeightType] = useState("auto");
+const PositionLayout = () => {
+  const [position, setPosition] = useState("relative");
 
   return (
     <PreviewLayout
-      widthType={widthType}
-      heightType={heightType}
-      widthValues={["auto", 300, "80%"]}
-      heightValues={["auto", 200, "60%"]}
-      setWidthType={setWidthType}
-      setHeightType={setHeightType}
+      label="position"
+      selectedValue={position}
+      values={["relative", "absolute"]}
+      setSelectedValue={setPosition}
     >
       <View
-        style={{
-          alignSelf: "flex-start",
-          backgroundColor: "aliceblue",
-          height: heightType,
-          width: widthType,
-          padding: 15,
-        }}
-      >
-        <View style={[styles.box, { backgroundColor: "powderblue" }]} />
-        <View style={[styles.box, { backgroundColor: "skyblue" }]} />
-        <View style={[styles.box, { backgroundColor: "steelblue" }]} />
-      </View>
+        style={[
+          styles.box,
+          {
+            top: 25,
+            left: 25,
+            position,
+            backgroundColor: "powderblue",
+          },
+        ]}
+      />
+      <View
+        style={[
+          styles.box,
+          {
+            top: 50,
+            left: 50,
+            position,
+            backgroundColor: "skyblue",
+          },
+        ]}
+      />
+      <View
+        style={[
+          styles.box,
+          {
+            top: 75,
+            left: 75,
+            position,
+            backgroundColor: "steelblue",
+          },
+        ]}
+      />
     </PreviewLayout>
   );
 };
 
 const PreviewLayout = ({
+  label,
   children,
-  widthType,
-  heightType,
-  widthValues,
-  heightValues,
-  setWidthType,
-  setHeightType,
+  values,
+  selectedValue,
+  setSelectedValue,
 }) => (
-  <SafeAreaView style={{ flex: 1, padding: 10, marginTop: 40 }}>
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={styles.label}>{label}</Text>
     <View style={styles.row}>
-      <Text style={styles.label}>width </Text>
-      {widthValues.map((value) => (
+      {values.map((value) => (
         <TouchableOpacity
           key={value}
-          onPress={() => setWidthType(value)}
-          style={[styles.button, widthType === value && styles.selected]}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}
         >
           <Text
             style={[
               styles.buttonLabel,
-              widthType === value && styles.selectedLabel,
+              selectedValue === value && styles.selectedLabel,
             ]}
           >
             {value}
@@ -66,31 +75,17 @@ const PreviewLayout = ({
         </TouchableOpacity>
       ))}
     </View>
-
-    <View style={styles.row}>
-      <Text style={styles.label}>height </Text>
-      {heightValues.map((value) => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setHeightType(value)}
-          style={[styles.button, heightType === value && styles.selected]}
-        >
-          <Text
-            style={[
-              styles.buttonLabel,
-              heightType === value && styles.selectedLabel,
-            ]}
-          >
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-    {children}
-  </SafeAreaView>
+    <View style={styles.container}>{children}</View>
+  </View>
 );
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 8,
+    backgroundColor: "aliceblue",
+    minHeight: 200,
+  },
   box: {
     width: 50,
     height: 50,
@@ -100,16 +95,18 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
   },
   button: {
-    padding: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
     borderRadius: 4,
     backgroundColor: "oldlace",
     alignSelf: "flex-start",
-    marginRight: 10,
-    marginBottom: 10,
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "48%",
+    textAlign: "center",
   },
   selected: {
     backgroundColor: "coral",
-    shadowOpacity: 0,
     borderWidth: 0,
   },
   buttonLabel: {
@@ -127,4 +124,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default WidthHeightBasics;
+export default PositionLayout;
