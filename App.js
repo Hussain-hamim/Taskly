8f61d9cfb70180799c514cf7966a84a86f3a34ad
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-24 20:50:42 +0430

    fadeIn fadeOut

--- App.js
+++ App.js
@@ -1,52 +1,74 @@
-import React, { useRef, useEffect } from "react";
-import { Animated, Text, View } from "react-native";
+import React, { useRef } from "react";
+import {
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+} from "react-native";
 
-const FadeInView = (props) => {
-  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+const App = () => {
+  // fadeAnim will be used as the value for opacity. Initial Value: 0
+  const fadeAnim = useRef(new Animated.Value(0)).current;
 
-  useEffect(() => {
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
     Animated.timing(fadeAnim, {
       toValue: 1,
-      duration: 10000,
+      duration: 5000,
       useNativeDriver: true,
     }).start();
-  }, [fadeAnim]);
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
-    <Animated.View // Special animatable View
-      style={{
-        ...props.style,
-        opacity: fadeAnim, // Bind opacity to animated value
-      }}
-    >
-      {props.children}
-    </Animated.View>
-  );
-};
-
-// // You can then use your `FadeInView` in place of a `View` in your components:
-const App = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <FadeInView
-        style={{
-          width: 250,
-          height: 50,
-          backgroundColor: "powderblue",
-        }}
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={[
+          styles.fadingContainer,
+          {
+            // Bind opacity to animated value
+            opacity: fadeAnim,
+          },
+        ]}
       >
-        <Text style={{ fontSize: 28, textAlign: "center", margin: 10 }}>
-          Fading in
-        </Text>
-      </FadeInView>
-    </View>
+        <Text style={styles.fadingText}>Fading View!</Text>
+      </Animated.View>
+      <View style={styles.buttonRow}>
+        <Button title="Fade In View" onPress={fadeIn} />
+        <Button title="Fade Out View" onPress={fadeOut} />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: "powderblue",
+  },
+  fadingText: {
+    fontSize: 28,
+  },
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: "space-evenly",
+    marginVertical: 16,
+  },
+});
 
 export default App;
