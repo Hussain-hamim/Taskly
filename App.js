d84a5513305e8dd5338f5e333174e9edd98a980d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-24 14:40:12 +0430

    URI Data Images

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import React from "react";
 import { Image } from "react-native";
 
-export const App = () => {
+const App = () => {
   return (
-    <Image
-      source={{
-        uri: "https://reactjs.org/logo-og.png",
-        method: "POST",
-        headers: {
-          Pragma: "no-cache",
-        },
-        body: "Your Body goes here",
-      }}
-      style={{ width: 400, height: 400 }}
-    />
+    <>
+      <Image
+        source={{
+          uri: "https://reactjs.org/logo-og.png",
+          method: "POST",
+          headers: {
+            Pragma: "no-cache",
+          },
+          body: "you body goes here",
+        }}
+      />
+
+      {/* // include at least width and height! */}
+      <Image
+        style={{
+          width: 51,
+          height: 51,
+          resizeMode: "contain",
+        }}
+        source={{
+          uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==",
+        }}
+      />
+      <Image
+        style={{
+          width: 51,
+          height: 51,
+          resizeMode: "contain",
+        }}
+        source={{
+          uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==",
+        }}
+      />
+    </>
   );
 };
+
+export default App;
