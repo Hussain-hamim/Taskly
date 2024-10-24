42e1eadaf27a4ae0b54b28c65326fcfc79f326fa
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-24 10:19:58 +0430

    Flex Basis, Grow, and Shrink

--- App.js
+++ App.js
@@ -1,102 +1,184 @@
 import React, { useState } from "react";
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+} from "react-native";
 
-const FlexWrapLayout = () => {
-  const [flexWrap, setFlexWrap] = useState("wrap");
+const App = () => {
+  const [powderblue, setPowderblue] = useState({
+    flexBasis: "auto",
+    flexShrink: 1,
+    flexGrow: 0,
+  });
+  const [skyblue, setSkyblue] = useState({
+    flexBasis: 100,
+    flexShrink: 0,
+    flexGrow: 1,
+  });
+  const [steelblue, setSteelblue] = useState({
+    flexBasis: 220,
+    flexShrink: 1,
+    flexGrow: 1,
+  });
 
   return (
-    <PreviewLayout
-      label="flexWrap"
-      selectedValue={flexWrap}
-      values={["wrap", "nowrap"]}
-      setSelectedValue={setFlexWrap}
-    >
-      <View style={[styles.box, { backgroundColor: "orangered" }]} />
-      <View style={[styles.box, { backgroundColor: "orange" }]} />
-      <View style={[styles.box, { backgroundColor: "mediumseagreen" }]} />
-      <View style={[styles.box, { backgroundColor: "deepskyblue" }]} />
-      <View style={[styles.box, { backgroundColor: "mediumturquoise" }]} />
-      <View style={[styles.box, { backgroundColor: "mediumslateblue" }]} />
-      <View style={[styles.box, { backgroundColor: "purple" }]} />
-    </PreviewLayout>
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.container,
+          {
+            flexDirection: "row",
+            alignContent: "space-between",
+          },
+        ]}
+      >
+        <BoxInfo color="powderblue" {...powderblue} setStyle={setPowderblue} />
+        <BoxInfo color="skyblue" {...skyblue} setStyle={setSkyblue} />
+        <BoxInfo color="steelblue" {...steelblue} setStyle={setSteelblue} />
+      </View>
+
+      <View style={styles.previewContainer}>
+        <View
+          style={[
+            styles.box,
+            {
+              flexBasis: powderblue.flexBasis,
+              flexGrow: powderblue.flexGrow,
+              flexShrink: powderblue.flexShrink,
+              backgroundColor: "powderblue",
+            },
+          ]}
+        />
+
+        <View
+          style={[
+            styles.box,
+            {
+              flexBasis: skyblue.flexBasis,
+              flexGrow: skyblue.flexGrow,
+              flexShrink: skyblue.flexShrink,
+              backgroundColor: "skyblue",
+            },
+          ]}
+        />
+        <View
+          style={[
+            styles.box,
+            {
+              flexBasis: steelblue.flexBasis,
+              flexGrow: steelblue.flexGrow,
+              flexShrink: steelblue.flexShrink,
+              backgroundColor: "steelblue",
+            },
+          ]}
+        />
+      </View>
+    </View>
   );
 };
 
-const PreviewLayout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}) => (
-  <View style={{ padding: 10, flex: 1 }}>
-    <Text style={styles.label}>{label}</Text>
-    <View style={styles.row}>
-      {values.map((value) => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[styles.button, selectedValue === value && styles.selected]}
-        >
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}
-          >
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
+const BoxInfo = ({ color, flexBasis, flexShrink, setStyle, flexGrow }) => (
+  <View style={[styles.row, { flexDirection: "column" }]}>
+    <View
+      style={[
+        styles.boxLabel,
+        {
+          backgroundColor: color,
+        },
+      ]}
+    >
+      <Text
+        style={{
+          color: "#fff",
+          fontWeight: "500",
+          textAlign: "center",
+        }}
+      >
+        Box
+      </Text>
     </View>
-    <View style={[styles.container, { [label]: selectedValue }]}>
-      {children}
-    </View>
+
+    <Text style={styles.label}>flexBasis</Text>
+    <TextInput
+      value={flexBasis}
+      style={styles.input}
+      onChangeText={(fB) =>
+        setStyle((value) => ({
+          ...value,
+          flexBasis: isNaN(parseInt(fB, 10)) ? "auto" : parseInt(fB, 10),
+        }))
+      }
+    />
+
+    <Text style={styles.label}>flexShrink</Text>
+    <TextInput
+      value={flexShrink}
+      style={styles.input}
+      onChangeText={(fS) =>
+        setStyle((value) => ({
+          ...value,
+          flexShrink: isNaN(parseInt(fS, 10)) ? undefined : parseInt(fS, 10),
+        }))
+      }
+    />
+    <Text style={styles.label}>flexGrow</Text>
+    <TextInput
+      value={flexGrow}
+      style={styles.input}
+      onChangeText={(fG) =>
+        setStyle((value) => ({
+          ...value,
+          flexGrow: isNaN(parseInt(fG, 10)) ? undefined : parseInt(fG, 10),
+        }))
+      }
+    />
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 8,
-    backgroundColor: "aliceblue",
-    maxHeight: 400,
+    paddingHorizontal: 10,
+    marginTop: 20,
   },
   box: {
+    flex: 1,
+    height: 50,
     width: 50,
-    height: 80,
   },
-  row: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
+  boxLabel: {
+    minWidth: 80,
+    padding: 8,
     borderRadius: 4,
-    backgroundColor: "oldlace",
-    marginHorizontal: "1%",
-    marginBottom: 6,
-    minWidth: "48%",
-    textAlign: "center",
-  },
-  selected: {
-    backgroundColor: "coral",
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: "500",
-    color: "coral",
-  },
-  selectedLabel: {
-    color: "white",
+    marginTop: 8,
   },
   label: {
-    textAlign: "center",
+    marginTop: 6,
+    fontSize: 16,
+    fontWeight: "100",
+  },
+  previewContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "aliceblue",
+    // marginTop: 100,
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
     marginBottom: 10,
-    fontSize: 24,
+  },
+  input: {
+    borderBottomWidth: 1,
+    paddingVertical: 3,
+    width: 50,
+    textAlign: "center",
   },
 });
 
-export default FlexWrapLayout;
+export default App;
