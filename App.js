2753ebe148a14517f44a9bd116e44eb7d80cc73b
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-26 10:38:47 +0430

    PanResponder with Animated Event Example

--- App.js
+++ App.js
@@ -1,105 +1,34 @@
-/* eslint-disable no-undef */
 import React, { useRef } from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  Text,
-  StyleSheet,
-  View,
-  ImageBackground,
-  Animated,
-  useWindowDimensions,
-} from "react-native";
-
-const images = new Array(6).fill(
-  "https://images.unsplash.com/photo-1556740749-887f6717d7e4"
-);
-
-const images2 = [
-  require("./assets/hhamim.jpg"),
-  require("./assets/landai.jpg"),
-  require("./assets/react_purple.png"),
-  require("./assets/landai.jpg"),
-  require("./assets/landai.jpg"),
-];
+import { Animated, View, StyleSheet, PanResponder, Text } from "react-native";
 
 const App = () => {
-  const scrollX = useRef(new Animated.Value(0)).current;
+  const pan = useRef(new Animated.ValueXY()).current;
 
-  const { width: windowWidth } = useWindowDimensions();
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: true,
+        }).start();
+      },
+    })
+  ).current;
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.scrollContainer}>
-        <ScrollView
-          horizontal={true}
-          pagingEnabled // the scroll stop on multiple scroll view size better for horizontal scrolling
-          showsHorizontalScrollIndicator={false}
-          onScroll={Animated.event(
-            [
-              {
-                nativeEvent: {
-                  contentOffset: {
-                    x: scrollX,
-                  },
-                },
-              },
-            ]
-            // { useNativeDriver: true } // Optional async listener
-          )}
-          scrollEventThrottle={1}
-        >
-          {images2.map((image, imageIndex) => {
-            return (
-              <View
-                style={{ width: windowWidth, height: 250 }}
-                key={imageIndex}
-              >
-                <ImageBackground
-                  source={image}
-                  style={[
-                    styles.card,
-                    {
-                      borderRadius: 10,
-                      borderWidth: 2,
-                      borderColor: "dimgray",
-                    },
-                  ]}
-                  resizeMode="cover"
-                >
-                  <View style={styles.textContainer}>
-                    <Text style={styles.infoText}>
-                      {"Image - " + (imageIndex + 1)}
-                    </Text>
-                  </View>
-                </ImageBackground>
-              </View>
-            );
-          })}
-        </ScrollView>
-
-        <View style={styles.indicatorContainer}>
-          {images.map((image, imageIndex) => {
-            const width = scrollX.interpolate({
-              inputRange: [
-                windowWidth * (imageIndex - 1),
-                windowWidth * imageIndex,
-                windowWidth * (imageIndex + 1),
-              ],
-              outputRange: [8, 25, 8],
-              extrapolate: "clamp",
-            });
-
-            return (
-              <Animated.View
-                key={imageIndex}
-                style={[styles.normalDot, { width }]}
-              />
-            );
-          })}
-        </View>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Drag & Release this box!</Text>
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }],
+        }}
+        {...panResponder.panHandlers}
+      >
+        <View style={styles.box} />
+      </Animated.View>
+    </View>
   );
 };
 
@@ -109,42 +38,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  scrollContainer: {
-    height: 300,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  card: {
-    flex: 1,
-    marginVertical: 4,
-    marginHorizontal: 16,
-    borderRadius: 8,
-    overflow: "hidden",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textContainer: {
-    backgroundColor: "rgba(0,0,0, 0.7)",
-    paddingHorizontal: 24,
-    paddingVertical: 8,
-    borderRadius: 5,
-  },
-  infoText: {
-    color: "white",
-    fontSize: 16,
+  titleText: {
+    fontSize: 14,
+    lineHeight: 24,
     fontWeight: "bold",
   },
-  normalDot: {
-    height: 8,
-    width: 8,
-    borderRadius: 4,
-    backgroundColor: "gray",
-    marginHorizontal: 4,
-  },
-  indicatorContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: "blue",
+    borderRadius: 5,
   },
 });
 
