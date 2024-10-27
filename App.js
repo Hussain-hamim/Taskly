2a11bc92fc0a92b07b6d1122154eb187c2de2689
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-27 23:08:30 +0430

    activity indicator

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
-const App = () => {
-  return <View></View>;
-};
+const App = () => (
+  <View style={[styles.container, styles.horizontal]}>
+    <ActivityIndicator />
+    <ActivityIndicator size="large" />
+    <ActivityIndicator size="small" color="#0000ff" />
+    <ActivityIndicator size="large" color="#00ff00" />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+});
 
 export default App;
