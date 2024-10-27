834e7021ef6e60674c28d086ad9e782fc9301f31
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-27 23:01:44 +0430

    clean up

--- App.js
+++ App.js
@@ -2,49 +2,7 @@ import React from "react";
 import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const App = () => {
-  return (
-    <>
-      <TouchableOpacity
-        accessible={true}
-        accessibilityLabel="Tap me!"
-        onPress={() => null}
-      >
-        <View>
-          <Text>Press me!</Text>
-        </View>
-      </TouchableOpacity>
-      <View>
-        <Text nativeID="formLabel">Label for Input Field</Text>
-        <TextInput
-          accessibilityLabel="input"
-          accessibilityLabelledBy="formLabel"
-          aria-valuemax={20}
-        />
-      </View>
-
-      <View
-        accessible={true}
-        accessibilityActions={[
-          { name: "cut", label: "cut" },
-          { name: "copy", label: "copy" },
-          { name: "paste", label: "paste" },
-        ]}
-        onAccessibilityAction={(event) => {
-          switch (event.nativeEvent.actionName) {
-            case "cut":
-              Alert.alert("Alert", "cut action success");
-              break;
-            case "copy":
-              Alert.alert("Alert", "copy action success");
-              break;
-            case "paste":
-              Alert.alert("Alert", "paste action success");
-              break;
-          }
-        }}
-      />
-    </>
-  );
+  return <View></View>;
 };
 
 export default App;
