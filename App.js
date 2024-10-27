841d859269bc7f15513c7201289e87d023fe2ea4
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-27 09:53:21 +0430

    secure storage libs

--- App.js
+++ App.js
@@ -1,1 +1,5 @@
 /** Async Storage is the React Native equivalent of Local Storage from the web */
+
+// some libraries to consider for secure storage:
+// 1. expo-secure-store
+// 2. react-native-keychain
