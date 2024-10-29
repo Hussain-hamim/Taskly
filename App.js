c93b911e6117101ee5cd938722879e20f34d8b56
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-29 10:15:22 +0430

    images

--- App.js
+++ App.js
@@ -1,175 +1,39 @@
-import React, { useState } from "react";
-import {
-  FlatList,
-  Platform,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-];
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity
-    onPress={onPress}
-    style={[styles.item, { backgroundColor }]}
-  >
-    <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-  </TouchableOpacity>
-);
-
-const App = () => {
-  const [selectedId, setSelectedId] = useState();
-
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "pink" : "green";
-    const color = item.id === selectedId ? "white" : "black";
-
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={backgroundColor}
-        textColor={color}
-      />
-    );
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
-        ItemSeparatorComponent={
-          Platform.OS === "android" &&
-          (({ highlighted }) => (
-            <View
-              style={[styles.separator, highlighted && { marginLeft: 0 }]}
-            />
-          ))
-        }
-        ListEmptyComponent={() => (
-          <Text
-            style={{
-              padding: 100,
-            }}
-          >
-            list is empty
-          </Text>
-        )}
-        ListFooterComponent={<Text>List footer comp</Text>}
-        ListFooterComponentStyle={{ backgroundColor: "gray" }}
-        ListHeaderComponent={<Text>List header comp</Text>}
-        ListHeaderComponentStyle={{ backgroundColor: "gray" }}
-        // columnWrapperStyle={{ color: "red" }} // not supported for single line column list
-        // getItemLayout={(data, index) => null}
-        initialNumToRender={2} // how many items to render in the initial batch
-        // initialScrollIndex={2}
-        // inverted={true}
-        horizontal={false}
-        key={({ item }) => item.id}
-        numColumns={2}
-        refreshing={true}
-        onRefresh={() => null}
-        // onViewableItemsChanged={() => null}
-        // progressViewOffset={}
-        // removeClippedSubviews={true}
-      />
-    </SafeAreaView>
-  );
-};
+/* eslint-disable no-undef */
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    paddingTop: 50,
   },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
-  title: {
-    fontSize: 32,
-  },
-  separator: {
-    borderWidth: 1,
-    borderColor: "#252622",
+  logo: {
+    width: 66,
+    height: 58,
   },
 });
 
-export default App;
+const DisplayAnImage = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.tinyLogo} source={require("./assets/hhamim.jpg")} />
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: "https://reactnative.dev/img/tiny_logo.png",
+        }}
+      />
+      <Image
+        style={styles.logo}
+        source={{
+          uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==",
+        }}
+      />
+    </View>
+  );
+};
+
+export default DisplayAnImage;
