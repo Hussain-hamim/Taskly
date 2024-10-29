37fac9a26db6a467ae99aa5961e62860050b5d46
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-29 10:36:08 +0430

    bg image

--- App.js
+++ App.js
@@ -1,51 +1,32 @@
-/* eslint-disable no-undef */
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+const image = require("./assets/hhamim.jpg");
+
+const App = () => (
+  <View style={styles.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Inside</Text>
+    </ImageBackground>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
+    flex: 1,
   },
-  tinyLogo: {
-    width: 100,
-    height: 100,
+  image: {
+    flex: 1,
+    justifyContent: "center",
   },
-  logo: {
-    width: 66,
-    height: 58,
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   },
 });
 
-const DisplayAnImage = () => {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.tinyLogo} source={require("./assets/hhamim.jpg")} />
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: "https://reactnative.dev/img/tiny_logo.png",
-        }}
-        alt="react native"
-        blurRadius={0.9}
-        crossOrigin="use-credentials"
-        defaultSource={null} // A static image to display while downloading the final image off the network.
-        fadeDuration={300}
-        height={150}
-        loadingIndicatorSource={null}
-        onError={null}
-        onLayout={null}
-        onLoad={null}
-        resizeMethod="auto"
-        resizeMode="cover"
-      />
-      <Image
-        style={styles.logo}
-        source={{
-          uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==",
-        }}
-      />
-    </View>
-  );
-};
-
-export default DisplayAnImage;
+export default App;
