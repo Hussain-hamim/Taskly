f425f4fb2e90725ad2cebd94a7da88753c50508c
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-30 20:15:09 +0430

    alternate sol

--- App.js
+++ App.js
@@ -1,150 +1,99 @@
-import React from "react";
+import { useRef, useState } from "react";
 import {
+  Animated,
+  ScrollView,
   StyleSheet,
   Text,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
 
-const App = () => {
+function ScrollExample() {
+  const [showScrollToTop, setshowScrollToTop] = useState(false);
+  const [pos, setpos] = useState(0);
+  const scrollViewRef = useRef(null);
+
+  console.log(showScrollToTop);
+
+  const handleScroll = (event) => {
+    const { y } = event.nativeEvent.contentOffset;
+    setshowScrollToTop(y > 1000); // show button if scroll down 200 pixels
+    setpos(y);
+  };
+
+  const scrollToTop = (scrollViewRef) => {
+    scrollViewRef?.scrollTo({ y: 0, animated: true });
+    // scrollViewRef.current?.scrollTo({ y: 0, animated: true });
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={{ flex: 1 }}>
       <ScrollView
-        onScroll={{
+        onScroll={({
           nativeEvent: {
-            contentInset: { button: 5, left: 5, right: 5, top: 5 },
-            contentOffset: { X: 0, y: 0 },
-            contentSize: { height: 10, width: 20 },
-            layoutMeasurement: { height: 20, width: 50 },
-            zoomScale: 0,
+            contentOffset: { y },
           },
-        }}
+        }) => setshowScrollToTop(y > 200)}
+        scrollEventThrottle={16}
+        ref={(ref) => (this.scrollViewRef = ref)}
       >
-        style={styles.scrollView}
-        StickyHeaderComponent=
-        {<Text style={styles.text}>this is sticky header</Text>}
-        contentContainerStyle={{}}
-        contentOffset={{ x: 0, y: 0 }} // starting scroll offset
-        decelerationRate="normal" disableIntervalMomentum={true} // stop at the
-        next index, good for pagination endFillColor={"yellow"}
-        fadingEdgeLength={10}
-        horizontal={false}
-        keyboardDismissMode="on-drag" keyboardShouldPersistTaps="always"
-        maintainVisibleContentPosition=
-        {{
-          minIndexForVisible: 3,
-          autoscrollToTopThreshold: 10,
-        }}
-        indicatorStyle="orchid" >
-        <TextInput placeholder="hello there" />
-        <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-          id est laborum.
-        </Text>
-        <Text style={[styles.text, { backgroundColor: "#17576c" }]}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pat non proident, sunt in minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-        <Text style={[styles.text, { backgroundColor: "red" }]}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pat non proident, sunt in minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-        <Text style={[styles.text, { backgroundColor: "green" }]}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pat non proident, sunt in minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-        <Text style={[styles.text, { backgroundColor: "#576857" }]}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pat non proident, sunt in minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-        <Text style={[styles.text, { backgroundColor: "#9b63b3" }]}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pat non proident, sunt in minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
+        {/* scrollable content */}
+        <View style={{ backgroundColor: "lightblue", padding: 30 }}>
+          <Text style={{ fontSize: 50 }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus architecto ducimus possimus corporis eveniet quod id
+            nulla quia. A laboriosam nostrum quaerat, consequuntur fuga magni
+            dignissimos velit, hic sunt pariatur quis voluptates dicta iusto
+            <Text style={{ color: "red" }}>{Math.floor(pos)} pxs</Text>
+            eaque porro qui veritatis autem rem explicabo non provident saepe
+            repellendus. Ullam, maxime! Ea, perspiciatis nemo. Voluptatibus
+            architecto ducimus possimus corporis eveniet quod id nulla quia. A
+            laboriosam nostrum quaerat, consequuntur fuga magni dignissimos
+            velit, hic sunt pariatur quis voluptates dicta iusto eaque porro qui
+            veritatis autem rem explicabo non provident saepe repellendus.
+            Ullam, maxime! Ea, perspiciatis nemo. Voluptatibus architecto
+            ducimus possimus corporis eveniet quod id nulla quia. A laboriosam
+            repellendus. Ullam, maxime! Ea, perspiciatis nemo. Voluptatibus
+            architecto ducimus possimus corporis eveniet quod id nulla quia. A
+            laboriosam nostrum quaerat, consequuntur fuga magni dignissimos
+            velit, hic sunt pariatur quis voluptates dicta iusto eaque porro qui
+            veritatis autem rem explicabo non provident saepe repellendus.
+            Ullam, maxime! Ea, perspiciatis nemo. Voluptatibus architecto
+            ducimus possimus corporis eveniet quod id nulla quia. A laboriosam
+            nostrum quaerat, consequuntur fuga magni dignissimos velit, hic sunt
+            pariatur quis voluptates dicta iusto eaque porro qui veritatis autem
+            rem explicabo non provident saepe repellendus. Ullam, maxime! Ea,
+            perspiciatis nemo.
+          </Text>
+        </View>
       </ScrollView>
-    </SafeAreaView>
+
+      {showScrollToTop && (
+        <TouchableOpacity
+          style={styles.scrollToTopButton}
+          onPress={() => scrollToTop(this.scrollViewRef)}
+          // onPress={scrollToTop}
+        >
+          <Text style={styles.scrollToTopText}> Top</Text>
+        </TouchableOpacity>
+      )}
+    </View>
   );
-};
+}
+
+export default ScrollExample;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight,
+  scrollToTopButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "gray",
+    padding: 10,
+    borderRadius: 5,
   },
-  scrollView: {
-    backgroundColor: "pink",
-    marginHorizontal: 20,
-  },
-  text: {
-    fontSize: 42,
+  scrollToTopText: {
+    color: "black",
+    fontWeight: "bold",
   },
 });
-
-export default App;
