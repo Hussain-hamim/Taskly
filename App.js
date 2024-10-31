5bca0cf8abeb29581b2a914a43543c5c00cad022
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-31 22:17:32 +0430

    nested text

--- App.js
+++ App.js
@@ -1,34 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { Text, StyleSheet } from "react-native";
 
-const TextInANest = () => {
-  const [titleText, setTitleText] = useState("Bird's Nest");
-  const bodyText = "This is not really a bird nest.";
-
-  const onPressTitle = () => {
-    setTitleText("Bird's Nest [pressed]");
-  };
-
+const BoldAndBeautiful = () => {
   return (
     <Text style={styles.baseText}>
-      <Text style={styles.titleText} onPress={onPressTitle}>
-        {titleText}
-        {"\n"}
-        {"\n"}
-      </Text>
-      <Text numberOfLines={5}>{bodyText}</Text>
+      I am bold
+      <Text style={styles.innerText}> and red</Text>
     </Text>
   );
 };
 
 const styles = StyleSheet.create({
   baseText: {
-    fontFamily: "Cochin",
-  },
-  titleText: {
-    fontSize: 20,
     fontWeight: "bold",
+  },
+  innerText: {
+    color: "red",
   },
 });
 
-export default TextInANest;
+export default BoldAndBeautiful;
