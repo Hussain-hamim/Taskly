7538c0817e846c312829a651bc98728bb5e873b5
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-31 22:22:02 +0430

    text and view container

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const BoldAndBeautiful = () => {
   return (
-    <Text style={styles.baseText}>
-      I am bold
-      <Text style={styles.innerText}> and red</Text>
-    </Text>
+    <>
+      <Text>
+        <Text>First part and </Text>
+        <Text>second part</Text>
+      </Text>
+      {/* // Text container: the text will be inline if the space allowed it //
+      |First part and second part| // otherwise, the text will flow as if it was
+      one // |First part | // |and second | // |part | */}
+      <View>
+        <Text>First part and </Text>
+        <Text>second part</Text>
+      </View>
+    </>
+    // View container: each text is its own block
+    // |First part and|
+    // |second part   |
+
+    // otherwise, the text will flow in its own block
+    // |First part |
+    // |and        |
+    // |second part|
   );
 };
 
