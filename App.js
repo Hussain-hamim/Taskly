40821fcbf467a7839cc63441b42c3f884858cfb1
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-31 22:43:53 +0430

    text props cont...

--- App.js
+++ App.js
@@ -3,45 +3,36 @@ import { Text, StyleSheet, View } from "react-native";
 
 const BoldAndBeautiful = () => {
   return (
-    <>
-      <Text>
-        <Text
-          accessibilityHint="a text"
-          accessibilityLanguage="english" // ios
-        >
-          First part and{" "}
-        </Text>
-        <Text>second part</Text>
+    <View style={styles.container}>
+      <Text
+        accessibilityHint="a text"
+        accessibilityLanguage="english" // ios
+        adjustsFontSizeToFit={true}
+        allowFontScaling={true}
+        android_hyphenationFrequency="none"
+        aria-busy={true}
+        aria-checked={true}
+        style={styles.text}
+        dataDetectorType="all" // "phoneNumber", "link", "email", "none"
+      >
+        First part and First part and First part and First part and First part
+        and First part and First part and First part and First part and First
+        part and First part and First part and First part and First part and
+        First part and{" "}
       </Text>
-      {/* // Text container: the text will be inline if the space allowed it //
-      |First part and second part| // otherwise, the text will flow as if it was
-      one // |First part | // |and second | // |part | */}
-      <View>
-        <Text>First part and </Text>
-        <Text>second part</Text>
-      </View>
-      {/* // View container: each text is its own block
-    // |First part and|
-    // |second part   |
-
-    // otherwise, the text will flow in its own block
-    // |First part |
-    // |and        |
-    // |second part| */}
-
-      {/* <View>
-  <MyAppText>
-    Text styled with the default font for the entire application
-  </MyAppText>
-  <MyAppHeaderText>Text styled as a header</MyAppHeaderText>
-</View> */}
-    </>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  baseText: {
-    fontWeight: "bold",
+  container: {
+    flex: 1,
+    backgroundColor: "#0e0d0df1",
+    paddingVertical: 30,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
   },
   innerText: {
     color: "red",
