9e4729957ebe0d0ebae5ca8d0f06dbf30be4cf6a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-02 18:27:46 +0430

    inputText

--- App.js
+++ App.js
@@ -1,54 +1,35 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
-const BoldAndBeautiful = () => {
+const TextInputExample = () => {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState("");
+
   return (
-    <View style={styles.container}>
-      <Text
-        accessibilityHint="a text"
-        accessibilityLanguage="english" // ios
-        adjustsFontSizeToFit={true}
-        allowFontScaling={true}
-        android_hyphenationFrequency="none"
-        aria-busy={true}
-        aria-checked={true}
-        style={styles.text}
-        dataDetectorType="all" // "phoneNumber", "link", "email", "none"
-        disabled={false}
-        ellipsizeMode="tail" // enum('head', 'middle', 'tail', 'clip') // On Android, when numberOfLines is set to a value higher than 1, only tail value will work correctly.
-        id="one"
-        nativeID="two"
-        onPress={() => null}
-        onLongPress={() => null}
-        onResponderMove={() => null}
-        onResponderGrant={() => null}
-        selectable={true}
-        selectionColor="orchid" // android
-      >
-        this is some text this is some text this is someeeeee text this is some
-        text this is some text this is some text this is some text this is some
-        text this is some text this is some text this is some text this is some
-        text this is some text this is some text this is some text this is some
-        text, study for exam so skipped the day, study for exam so skipped the
-        day
-      </Text>
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0e0d0df1",
-    paddingVertical: 30,
-  },
-  text: {
-    color: "white",
-    fontSize: 20,
-  },
-  innerText: {
-    color: "red",
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
-export default BoldAndBeautiful;
+export default TextInputExample;
