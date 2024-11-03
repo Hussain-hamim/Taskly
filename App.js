8c057eb789e848e83378f8cae30deb52e78634bb
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-03 21:05:20 +0430

    view

--- App.js
+++ App.js
@@ -1,45 +1,20 @@
-import React, { useState } from "react";
-import { StyleSheet, TouchableWithoutFeedback, Text, View } from "react-native";
+import React from "react";
+import { View, Text } from "react-native";
 
-const TouchableWithoutFeedbackExample = () => {
-  const [count, setCount] = useState(0);
-
-  const onPress = () => {
-    setCount(count + 1);
-  };
-
+const ViewBoxesWithColorAndText = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.countContainer}>
-        <Text style={styles.countText}>Count: {count}</Text>
-      </View>
-      <TouchableWithoutFeedback onPress={onPress}>
-        <View style={styles.button}>
-          <Text>Touch Here</Text>
-        </View>
-      </TouchableWithoutFeedback>
+    <View
+      style={{
+        flexDirection: "row",
+        height: 100,
+        padding: 20,
+      }}
+    >
+      <View style={{ backgroundColor: "blue", flex: 0.3 }} />
+      <View style={{ backgroundColor: "red", flex: 0.5 }} />
+      <Text>Hello World!</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 10,
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10,
-  },
-  countContainer: {
-    alignItems: "center",
-    padding: 10,
-  },
-  countText: {
-    color: "#FF00FF",
-  },
-});
-
-export default TouchableWithoutFeedbackExample;
+export default ViewBoxesWithColorAndText;
