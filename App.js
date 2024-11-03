d32e624a2ef8c51c1327c9b0189210b7ea4140c0
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-03 21:29:35 +0430

    view props

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 
 const ViewBoxesWithColorAndText = () => {
   return (
@@ -9,7 +9,15 @@ const ViewBoxesWithColorAndText = () => {
         height: 100,
         padding: 20,
       }}
+      collapsable
+      focusable
+      hitSlop={{ bottom: 10, right: 0, top: 10, left: 0 }}
+      needsOffscreenAlphaCompositing
+      // onMoveShouldSetResponder={{ nativeEvent: { pressEvent: true } }}
+      // onResponderGrant={}
+      // onResponderMove={}
     >
+      <StatusBar barStyle="default" backgroundColor={"orchid"} />
       <View style={{ backgroundColor: "blue", flex: 0.3 }} />
       <View style={{ backgroundColor: "red", flex: 0.5 }} />
       <Text>Hello World!</Text>
