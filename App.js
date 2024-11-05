328901c9461f5041a4975fd8300b9260263653b6
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-05 20:36:48 +0430

    Working with animations

--- App.js
+++ App.js
@@ -1,84 +1,57 @@
 import React, { useRef } from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  Text,
-  StyleSheet,
-  View,
-  ImageBackground,
   Animated,
-  useWindowDimensions,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  Alert,
 } from "react-native";
 
-// const images = new Array(6).fill());
-const images = [
-  require("./assets/hhamim.jpg"),
-  require("./assets/hhamim.jpg"),
-  require("./assets/hhamim.jpg"),
-  require("./assets/hhamim.jpg"),
-  require("./assets/hhamim.jpg"),
-  require("./assets/hhamim.jpg"),
-];
-
 const App = () => {
-  const scrollX = useRef(new Animated.Value(0)).current;
+  // fadeAnim will be used as the value for opacity. Initial Value: 0
+  const fadeAnim = useRef(new Animated.Value(0)).current;
 
-  const { width: windowWidth } = useWindowDimensions();
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start(({ finished }) => {
+      /* completion callback */
+      console.log(finished);
+      finished && Alert.alert("do not stop animation please");
+      finished === false && console.log("do not stop animation please");
+    });
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.scrollContainer}>
-        <ScrollView
-          horizontal={true}
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          onScroll={Animated.event([
-            {
-              nativeEvent: {
-                contentOffset: {
-                  x: scrollX,
-                },
-              },
-            },
-          ])}
-          scrollEventThrottle={1}
-        >
-          {images.map((image, imageIndex) => {
-            return (
-              <View
-                style={{ width: windowWidth, height: 250 }}
-                key={imageIndex}
-              >
-                <ImageBackground source={image} style={styles.card}>
-                  <View style={styles.textContainer}>
-                    <Text style={styles.infoText}>
-                      {"Image - " + imageIndex}
-                    </Text>
-                  </View>
-                </ImageBackground>
-              </View>
-            );
-          })}
-        </ScrollView>
-        <View style={styles.indicatorContainer}>
-          {images.map((image, imageIndex) => {
-            const width = scrollX.interpolate({
-              inputRange: [
-                windowWidth * (imageIndex - 1),
-                windowWidth * imageIndex,
-                windowWidth * (imageIndex + 1),
-              ],
-              outputRange: [8, 16, 8],
-              extrapolate: "clamp",
-            });
-            return (
-              <Animated.View
-                key={imageIndex}
-                style={[styles.normalDot, { width }]}
-              />
-            );
-          })}
-        </View>
+      <Animated.View
+        style={[
+          styles.fadingContainer,
+          {
+            // Bind opacity to animated value
+            opacity: fadeAnim,
+          },
+        ]}
+      >
+        <Text style={styles.fadingText}>Fading View!</Text>
+      </Animated.View>
+      <View style={styles.buttonRow}>
+        <Button title="Fade In View" onPress={fadeIn} />
+        <Button title="Fade Out View" onPress={fadeOut} />
       </View>
     </SafeAreaView>
   );
@@ -90,42 +63,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  scrollContainer: {
-    height: 300,
-    alignItems: "center",
-    justifyContent: "center",
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: "powderblue",
   },
-  card: {
-    flex: 1,
-    marginVertical: 4,
-    marginHorizontal: 16,
-    borderRadius: 5,
-    overflow: "hidden",
-    alignItems: "center",
-    justifyContent: "center",
+  fadingText: {
+    fontSize: 28,
   },
-  textContainer: {
-    backgroundColor: "rgba(0,0,0, 0.7)",
-    paddingHorizontal: 24,
-    paddingVertical: 8,
-    borderRadius: 5,
-  },
-  infoText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  normalDot: {
-    height: 8,
-    width: 8,
-    borderRadius: 4,
-    backgroundColor: "silver",
-    marginHorizontal: 4,
-  },
-  indicatorContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: "space-evenly",
+    marginVertical: 16,
   },
 });
 
