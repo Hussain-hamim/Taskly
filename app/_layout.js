6869077d2e8303780e5f08fa23db74ef9b86a2e1
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-12 21:47:10 +0430

    stack navigation

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,16 @@ import { Stack } from "expo-router";
 function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "shopping list" }} />
+      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen name="idea" options={{ title: "Idea" }} />
     </Stack>
   );
 }
