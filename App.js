be014cbbb08e8425374a778cc17a901b182c7ff3
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-12 10:50:01 +0430

    start taskly app

--- App.js
+++ App.js
@@ -1,60 +1,13 @@
 /* eslint-disable react/jsx-key */
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, Dimensions } from "react-native";
-
-const windowDimensions = Dimensions.get("window");
-const screenDimensions = Dimensions.get("screen");
-
-const windowDimension2 = Dimensions.get("window");
-const screenDimension2 = Dimensions.get("screen");
+import React from "react";
+import { View, StyleSheet, Text, Dimensions, PixelRatio } from "react-native";
 
 const App = () => {
-  const [dimensions, setDimensions] = useState({
-    window: windowDimensions,
-    screen: screenDimensions,
-  });
-
-  const [dime, setdime] = useState({
-    window: windowDimension2,
-    screen: screenDimension2,
-  });
-
-  useEffect(() => {
-    const subscription = Dimensions.addEventListener(
-      "change",
-      ({ window, screen }) => {
-        setDimensions({ window, screen });
-      }
-    );
-    return () => subscription?.remove();
-  });
-
-  useEffect(() => {
-    const sub = Dimensions.addEventListener("change", ({ window, screen }) => {
-      setdime({ window, screen });
-    });
-
-    return () => {
-      sub?.remove();
-    };
-  });
-
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Window Dimensions</Text>
-
-      {Object.entries(dimensions.window).map(([key, value]) => (
-        <Text>
-          {key} - {value}
-        </Text>
-      ))}
-
-      <Text style={styles.header}>Screen Dimensions</Text>
-      {Object.entries(dimensions.screen).map(([key, value]) => (
-        <Text>
-          {key} - {value}
-        </Text>
-      ))}
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
     </View>
   );
 };
@@ -62,13 +15,16 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center",
   },
-  header: {
-    fontSize: 16,
-    marginVertical: 10,
+  itemContainer: {
+    borderBottomColor: "#1a759f",
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
   },
+  itemText: { fontSize: 18, fontWeight: "200" },
 });
 
 export default App;
