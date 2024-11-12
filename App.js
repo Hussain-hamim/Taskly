a8b669a6beea5520aa6aef0d176a6d5b05ab5230
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-12 11:24:50 +0430

    making the delete button

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-/* eslint-disable react/jsx-key */
 import React from "react";
-import { View, StyleSheet, Text, Dimensions, PixelRatio } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Dimensions,
+  PixelRatio,
+  Pressable,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { theme } from "./theme";
 
 const App = () => {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "it will be gone for good",
+      [
+        { text: "Cancel", onPress: () => null, style: "cancel" },
+        { text: "Yes", onPress: () => null, style: "destructive" },
+      ]
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={handleDelete}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -23,8 +50,22 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     paddingHorizontal: 8,
     paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   itemText: { fontSize: 18, fontWeight: "200" },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
 });
 
 export default App;
