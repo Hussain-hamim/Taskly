1c6408ba7f72a2b30b1db868b9ea9f1a1a66fbea
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-16 21:28:38 +0430

    push notification

--- app/counter/index.js
+++ app/counter/index.js
@@ -1,12 +1,44 @@
 import { useRouter } from "expo-router";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Device from "expo-device";
+import * as Notification from "expo-notifications";
 
 export default function CounterScreen() {
   const router = useRouter();
 
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotificationsAsync();
+    if (result === "granted") {
+      await Notification.scheduleNotificationAsync({
+        content: {
+          title: "I'm notification from Hussain's app",
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+      console.log(result);
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notification",
+          "Enable the notification permission for expo go in settings"
+        );
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={scheduleNotification}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -20,5 +52,16 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
